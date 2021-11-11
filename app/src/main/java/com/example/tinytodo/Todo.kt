fba49c1177f5109ba16fce5bc3a9a64eedc882ce
Yu357 <forestgreen14689@gmail.com>
2021-11-11 15:04:48 +0900

    Todoモデル定義

---
+++ app/src/main/java/com/example/tinytodo/Todo.kt
@@ -0,0 +1,14 @@
+package com.example.tinytodo
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+open class Todo : RealmObject() {
+    @PrimaryKey
+    var id: Int = 0
+    var content: String = ""
+    var isPinned: Boolean = false
+    var isAchieved: Boolean = false
+    var achievedDate: Date? = null
+}
