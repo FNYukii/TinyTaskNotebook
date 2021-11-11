38dc70735075ccef96567c96b56483988be6398f
Yu357 <forestgreen14689@gmail.com>
2021-11-11 14:17:18 +0900

    ボトムナビゲーションメニューを実装

--- app/src/main/java/com/example/tinytodo/MainActivity.kt
+++ app/src/main/java/com/example/tinytodo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.tinytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
     }
 }
