6d949c747f5251c2da667c220ba29a20fa49063e
Yu357 <forestgreen14689@gmail.com>
2021-11-11 17:32:30 +0900

    RealmRecyclerViewAdapterで自動更新できた

--- app/src/main/java/com/example/tinytodo/TodoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/tinytodo/TodoRecyclerViewAdapter.kt
@@ -5,12 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
 import io.realm.RealmResults
 import kotlinx.android.synthetic.main.one_todo.view.*
 
 class TodoRecyclerViewAdapter(
-    private val todos: RealmResults<Todo>
-): RecyclerView.Adapter<TodoRecyclerViewAdapter.CustomViewHolder>(){
+    private val todos: OrderedRealmCollection<Todo>
+): RealmRecyclerViewAdapter<Todo, TodoRecyclerViewAdapter.CustomViewHolder>(todos, true){
 
     class CustomViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val todoContentText: TextView = itemView.todoContentText
